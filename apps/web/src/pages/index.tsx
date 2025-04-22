aadd034f5990c0703bacd718b4572ac51d840d05
xenomech <gokulsmenon227@gmail.com>
2025-04-23 02:41:28 +0530

    feat: refactor to ready to develop setup

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "@workspace/ui/components/button";
+export default function Home() {
+  return (
+    <div className="bg-red-500">
+      <Button>Click me</Button>
+    </div>
+  );
+}
