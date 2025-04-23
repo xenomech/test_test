7cd61c817df2f597a7d8bf88625d0b43a06e72fe
xenomech <gokulsmenon227@gmail.com>
2025-04-23 14:57:55 +0530

    feat: add compose and builders for fe and be

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["@workspace/ui"],
+  output: "export",
+  distDir: "dist",
+};
 
 export default nextConfig;
