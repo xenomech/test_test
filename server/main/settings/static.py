d302a4c9f532f2731780e6dd68f94ceab847102a
xenomech <gokulsmenon227@gmail.com>
2025-05-11 23:45:14 +0530

    feat: deployment updates

--- server/main/settings/static.py
+++ server/main/settings/static.py
@@ -1,5 +1,12 @@
 from .base import BASE_DIR
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
